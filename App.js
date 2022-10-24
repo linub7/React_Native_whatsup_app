577319a314afaddf9fd6deb0316b89952a690969
a1a2a3a4a5 <linub7@gmail.com>
2022-10-24 12:48:43 +0330

    add roboto font - load font

--- App.js
+++ App.js
@@ -1,12 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useCallback, useEffect, useState } from 'react';
+import { StyleSheet, Text } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [appIsLoaded, setAppIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const prepare = async () => {
+      try {
+        await Font.loadAsync({
+          black: require('./assets/fonts/Roboto-Black.ttf'),
+          blackItalic: require('./assets/fonts/Roboto-BlackItalic.ttf'),
+          bold: require('./assets/fonts/Roboto-Bold.ttf'),
+          boldItalic: require('./assets/fonts/Roboto-BoldItalic.ttf'),
+          italic: require('./assets/fonts/Roboto-Italic.ttf'),
+          light: require('./assets/fonts/Roboto-Light.ttf'),
+          lightItalic: require('./assets/fonts/Roboto-LightItalic.ttf'),
+          medium: require('./assets/fonts/Roboto-Medium.ttf'),
+          mediumItalic: require('./assets/fonts/Roboto-MediumItalic.ttf'),
+          regular: require('./assets/fonts/Roboto-Regular.ttf'),
+          thin: require('./assets/fonts/Roboto-Thin.ttf'),
+          thinItalic: require('./assets/fonts/Roboto-ThinItalic.ttf'),
+        });
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setAppIsLoaded(true);
+      }
+    };
+
+    prepare();
+  }, []);
+
+  const onLayout = useCallback(async () => {
+    if (appIsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsLoaded]);
+
+  if (!appIsLoaded) return null;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider style={styles.container} onLayout={onLayout}>
+      <SafeAreaView>
+        <Text style={styles.label}>Hi Everyone!</Text>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
@@ -14,7 +57,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  label: {
+    color: 'black',
+    fontSize: 18,
+    fontFamily: 'regular',
   },
 });
